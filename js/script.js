e11310dda3598b99a7ac526eee55e6906f9eeda6
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2022-12-27 21:01:07 +0100

    fixed menu-bar and refactored changing pictures

--- js/script.js
+++ js/script.js
@@ -5,20 +5,22 @@ const backgroundNav = document.querySelector('.background-navigation');
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('.navigation-link');
 const tvImages = document.querySelectorAll('.tv-img');
-const imgInterview = document.querySelector('.interview-img');
-const imgBooking = document.querySelector('.hotel-img');
-const imgOrdering = document.querySelector('.ordering-img');
-const img911 = document.querySelector('.emergency-img');
-
 const remoteBtn = document.querySelectorAll('.remote-btn');
+const img = {
+	imgInterview: document.querySelector('.interview-img'),
+	imgBooking: document.querySelector('.hotel-img'),
+	imgOrdering: document.querySelector('.ordering-img'),
+	img911: document.querySelector('.emergency-img'),
+};
 
 const startAnimationShowUp = () => {
 	headingText.classList.add('animation-show-up');
 	btnHeader.classList.add('animation-show-up');
 };
 setTimeout(startAnimationShowUp, 2000);
+
 burgerIcon.addEventListener('click', () => {
-	burgerIcon.classList.toggle('open')
+	burgerIcon.classList.toggle('open');
 	if (backgroundNav.classList.contains('active')) {
 		backgroundNav.classList.remove('active');
 		backgroundNav.classList.add('dis-active');
@@ -33,33 +35,27 @@ burgerIcon.addEventListener('click', () => {
 		}, 500);
 	}
 });
+
 navLinks.forEach((link) => {
 	link.addEventListener('click', () => {
 		backgroundNav.classList.remove('active');
 		backgroundNav.classList.add('dis-active');
+		burgerIcon.classList.remove('open');
 		setTimeout(function removeLink() {
 			nav.style.display = 'none';
 		}, 300);
 	});
 });
-
 remoteBtn.forEach((element) => {
-	element.addEventListener('click', () => {
+	element.addEventListener('click', (e) => {
 		tvImages.forEach((element) => {
 			element.classList.remove('active-img');
 		});
-		remoteBtn.forEach(element=>{
-			element.classList.remove('active-btn')
-		})
-		if (element.classList.contains('interview-btn')) {
-			imgInterview.classList.add('active-img');
-		} else if (element.classList.contains('booking-btn')) {
-			imgBooking.classList.add('active-img');
-		} else if (element.classList.contains('ordering-btn')) {
-			imgOrdering.classList.add('active-img');
-		} else if (element.classList.contains('emergency-btn')) {
-			img911.classList.add('active-img');
-		}
+		remoteBtn.forEach((element) => {
+			element.classList.remove('active-btn');
+		});
+		const chosenButton = e.target.getAttribute('data-name');
+		img[chosenButton].classList.add('active-img');
 		element.classList.add('active-btn');
 	});
 });
